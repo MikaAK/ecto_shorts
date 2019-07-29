b091871254b26a564662d9d6488e2aeaecaf4149
MikaAK <mika@kalathil.me>
2019-07-29 02:06:06 +0200

    bump: 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoShorts.MixProject do
   def project do
     [
       app: :ecto_shorts,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
