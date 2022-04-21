0f3684766d1e10f6806287dae0fe61c43cd17287
Mayel de Borniol <git@mayel.space>
2022-04-22 08:09:43 +1200

    c

--- mix.exs
+++ mix.exs
@@ -15,11 +15,7 @@ defmodule EctoShorts.MixProject do
   end
 
   # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
+  def application, do: [ extra_applications: [:logger] ]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
