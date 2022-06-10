01e5f281aa9037044c6ba3c8cc6a5a0e7dac5ffb
MikaAK <mika@kalathil.me>
2022-06-09 21:11:15 -0700

    chore: bump to 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoShorts.MixProject do
   def project do
     [
       app: :ecto_shorts,
-      version: "1.1.5",
+      version: "2.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
