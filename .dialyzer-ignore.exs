96f6e8a4f444c3c9036d355416c1edf0ef9c0329
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-07-31 07:50:06 -0700

    clean up dialyzer filters

--- .dialyzer-ignore.exs
+++ .dialyzer-ignore.exs
@@ -1,7 +1,5 @@
 [
-  ~r|lib/actions.ex:40:invalid_contract Invalid type specification for function all.|,
   ~r|lib/actions.ex:62:overlapping_contract The contract for all/2 is overloaded.|,
-  ~r|lib/actions.ex:90:invalid_contract Invalid type specification for function all.|,
   ~r|lib/actions.ex:168:invalid_contract Invalid type specification for function create.|,
   ~r|lib/actions.ex:169:invalid_contract Invalid type specification for function create.|,
   ~r|lib/actions.ex:189:invalid_contract Invalid type specification for function find_or_create.|,
@@ -12,13 +10,6 @@
   ~r|lib/actions.ex:275:overlapping_contract The contract for update/3 is overloaded.|,
   ~r|lib/actions.ex:355:call The function call delete will not succeed.|,
   ~r|lib/actions.ex:411:invalid_contract Invalid type specification for function delete.|,
-  ~r|lib/actions.ex:418:invalid_contract Invalid type specification for function stream.|,
-  ~r|lib/actions.ex:419:invalid_contract Invalid type specification for function stream.|,
-  ~r|lib/actions.ex:428:invalid_contract Invalid type specification for function aggregate.|,
-  ~r|lib/actions.ex:435:invalid_contract Invalid type specification for function aggregate.|,
   ~r|lib/common_changes.ex:.*:exact_eq The test maybe_improper_list()|,
   ~r|lib/common_changes.ex:149:call The function call preload_changeset_assoc will not succeed.|,
-  # ~r|lib/query_builder/common.ex:78:call The function call function_exported will not succeed.|,
-  # ~r|lib/query_builder/schema.ex:17:no_return Function create_schema_filter/2 has no local return.|
-  #~r|lib/query_builder/schema.ex:20:call The function call create_schema_filter will not succeed|
   ]
