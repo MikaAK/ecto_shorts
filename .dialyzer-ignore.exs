bcd2a2916e19c0c2eea3de6c289ef5f9185c15d4
CorneliaKelinske <corneliakelinske@gmail.com>
2022-11-13 14:06:30 -0800

    fix remaining actions typespecs (#31)

--- .dialyzer-ignore.exs
+++ .dialyzer-ignore.exs
@@ -1,8 +1,4 @@
 [
-  ~r|lib/actions.ex:.*:invalid_contract Invalid type specification for function find_and_update.|,
-  ~r|lib/actions.ex:.*:invalid_contract Invalid type specification for function find_and_upsert.|,
-  ~r|lib/actions.ex:.*:call The function call delete will not succeed.|,
-  ~r|lib/actions.ex:.*:invalid_contract Invalid type specification for function delete.|,
   ~r|lib/common_changes.ex:.*:exact_eq The test maybe_improper_list()|,
   ~r|lib/common_changes.ex:.*:call The function call preload_changeset_assoc will not succeed.|,
   ]
