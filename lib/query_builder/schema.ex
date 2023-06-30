b3c9c540512b360ef8a1cebd57d861d948d93d2f
hlista <hlista91@gmail.com>
2023-06-30 12:00:57 -0500

    updating schema.ex to follow named function pattern

--- lib/query_builder/schema.ex
+++ lib/query_builder/schema.ex
@@ -25,36 +25,47 @@ defmodule EctoShorts.QueryBuilder.Schema do
   def create_schema_filter({filter_field, val}, schema, query) do
     cond do
       filter_field in schema.__schema__(:query_fields) ->
-        case schema.__schema__(:type, filter_field) do
-          {:array, _} ->
-            ComparisonFilter.build_array(query, schema.__schema__(:field_source, filter_field), val)
-          _ ->
-            ComparisonFilter.build(query, schema.__schema__(:field_source, filter_field), val)
-        end
+        create_schema_query_field_filter(query, schema, filter_field, val)
 
       filter_field in schema.__schema__(:associations) ->
         %{queryable: relational_schema} = schema.__schema__(:association, filter_field)
-        binding_alias = :"ecto_shorts_#{filter_field}"
-
-        query = Ecto.Query.join(
-          query,
-          :inner,
-          [scm],
-          assoc in assoc(scm, ^filter_field)
-        )
-
-        query = %{query |
-          aliases: add_relational_alias(query, binding_alias),
-          joins: add_join_alias(query, filter_field, binding_alias)
-        }
-
-        ComparisonFilter.build_relational(query, binding_alias, val, relational_schema)
-
+        create_schema_assocation_filter(query, filter_field, val, schema, relational_schema)
       true ->
         Logger.debug("[EctoShorts] #{Atom.to_string(filter_field)} is not a field for #{schema.__schema__(:source)} where filter")
 
         query
     end
+  end
+
+  defp create_schema_query_field_filter(query, schema, filter_field, val) do
+    case schema.__schema__(:type, filter_field) do
+      {:array, _} ->
+        ComparisonFilter.build_array(query, schema.__schema__(:field_source, filter_field), val)
+      _ ->
+        ComparisonFilter.build(query, schema.__schema__(:field_source, filter_field), val)
+    end
+  end
+
+  defp create_schema_assocation_filter(_query, filter_field, _val, schema, nil) do
+    raise ArgumentError, message: "#{Atom.to_string(filter_field)} is neither a field nor has a valid association for #{schema.__schema__(:source)}"
+  end
+
+  defp create_schema_assocation_filter(query, filter_field, val, _schema, relational_schema) do
+    binding_alias = :"ecto_shorts_#{filter_field}"
+
+    query = Ecto.Query.join(
+      query,
+      :inner,
+      [scm],
+      assoc in assoc(scm, ^filter_field)
+    )
+
+    query = %{query |
+      aliases: add_relational_alias(query, binding_alias),
+      joins: add_join_alias(query.joins, filter_field, binding_alias)
+    }
+
+    ComparisonFilter.build_relational(query, binding_alias, val, relational_schema)
   end
 
   defp add_relational_alias(query, new_alias) do
@@ -66,8 +77,8 @@ defmodule EctoShorts.QueryBuilder.Schema do
     end
   end
 
-  defp add_join_alias(query, filter_field, binding_alias) do
-    Enum.map(query.joins, fn join ->
+  defp add_join_alias(joins, filter_field, binding_alias) do
+    Enum.map(joins, fn join ->
       if elem(join.assoc, 1) === filter_field do
         %{join | as: binding_alias}
       else
