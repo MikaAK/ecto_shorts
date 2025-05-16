19f23c1972513c6ed2cce8be2ef9bdfb10f62045
Kurt Hogarth <87607684+cylkdev@users.noreply.github.com>
2025-05-16 12:16:33 -0700

    chore: remove key from config

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,6 @@ import Config
 
 config :ecto_shorts,
   mix_env: Mix.env(),
-  show_sensitive_data: Mix.env() in [:dev, :test],
   repo: nil,
   replica: nil,
   error_module: EctoShorts.Actions.Error
